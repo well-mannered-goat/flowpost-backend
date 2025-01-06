e3bfa01ed0350f86cd6e676e6eb1479c1260d799
neetance <ankitbasu14@gmail.com>
2025-01-06 23:28:41 +0530

    created the endpoint and added the method for reels uploading

--- src/controllers/PostControllers.ts
+++ src/controllers/PostControllers.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { PostImageIG } from "../models/PostModels";
+import { PostImageIG, PostReelIG } from "../models/PostModels";
 import { User } from "../models/UserModel";
 import { CustomRequest } from "../middlewares/AuthenticateToken";
 import axios, { AxiosResponse } from "axios";
@@ -11,7 +11,7 @@ interface APIResponse {
 
 export const createPostImageIG = async (req: CustomRequest, res: Response) => {
     try {
-        const { url, caption, userTags } = req.body;
+        const { url, caption, userTags, isCarousel, locationId } = req.body;
 
         const user = await User.findById(req.user)
         const token = user?.insta_access_token;
@@ -54,15 +54,77 @@ export const createPostImageIG = async (req: CustomRequest, res: Response) => {
         const publishRes = await axios.post(publishUrl);
 
         const mediaId = publishRes.data.id;
-
         if (!mediaId) {
             res.status(400).json({ message: "Error publishing post." });
             return;
         }
 
         res.status(201).json({ message: "Post created successfully", mediaId });
-    } catch (error) {
+    }
+    catch (error) {
         console.error("Error creating Instagram post:", error);
         res.status(500).json({ message: "Internal server error", error });
     }
 };
+
+export const createPostReelIG = async (req: CustomRequest, res: Response) => {
+    try {
+        const { url, caption, userTags, coverUrl, audioName, shareToFeed, collaborators, locationId } = req.body;
+
+        const user = await User.findById(req.user);
+        const token = user?.insta_access_token;
+
+        if (!token) {
+            res.status(404).json({ message: "Access token not found" });
+            return;
+        }
+
+        const accessToken = decryptToken(token);
+        const userInfoUrl = `https://graph.facebook.com/v21.0/me/accounts?access_token=${accessToken}`;
+        const userInfoRes = await axios.get(userInfoUrl);
+
+        const igAccount = userInfoRes.data.data.find(
+            (account: any) => account.access_token === accessToken
+        );
+
+        if (!igAccount) {
+            res.status(400).json({ message: "Invalid access token or no associated Instagram account." });
+            return;
+        }
+
+        const igUserId = igAccount.id;
+        const newPost = new PostReelIG({
+            url,
+            caption,
+            userTags,
+            accessToken,
+            shareToFeed,
+            collaborators,
+            coverUrl,
+            audioName,
+            locationId,
+            user
+        });
+        await newPost.save();
+
+        const uploadurl = `POST https://graph.facebook.com/v21.0/<YOUR_APP_USERS_INSTAGRAM_USER_ID>/media?media_type=REELS&video_url=${url}&caption=${caption}&share_to_feed=${shareToFeed}&collaborators=${collaborators}&cover_url=${coverUrl}&audio_name=${audioName}&user_tags=${userTags}&location_id=${locationId}&access_token=${accessToken}`;
+        const uploadRes = await axios.post(uploadurl);
+
+        const containerId = uploadRes.data.id;
+
+        const publishUrl = `https://graph.facebook.com/v21.0/${igUserId}/media_publish?creation_id=${containerId}&access_token=${accessToken}`;
+        const publishRes = await axios.post(publishUrl);
+
+        const mediaId = publishRes.data.id;
+        if (!mediaId) {
+            res.status(400).json({ message: "Error publishing post." });
+            return;
+        }
+
+        res.status(201).json({ message: "Reel created successfully", mediaId });
+    }
+    catch (error) {
+        console.error("Error creating Instagram reel:", error);
+        res.status(500).json({ message: "Internal server error", error });
+    }
+}
--- src/routes/PostRoutes.ts
+++ src/routes/PostRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createPostImageIG } from "../controllers/PostControllers";
+import { createPostImageIG, createPostReelIG } from "../controllers/PostControllers";
 import { storeAccessToken } from "../controllers/TokenController";
 import authenticateToken from "../middlewares/AuthenticateToken";
 import uploadToCloud from "../middlewares/cloudStorage";
@@ -9,6 +9,7 @@ const router = Router();
 const upload = multer({ dest: "uploads/" });
 
 router.post("/ig/image", authenticateToken, upload.single("image"), uploadToCloud, createPostImageIG);
+router.post("/ig/reel", authenticateToken, upload.single("video"), uploadToCloud, createPostReelIG);
 router.post("/token", authenticateToken, storeAccessToken);
 
 export default router;
